4db036a703a4b34fbdf640990ccee6e91210377e
nemixu <s.r.seagrave@gmail.com>
2021-03-09 21:05:58 +0000

    type conversions

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,19 +6,17 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            byte number = 1;
-            int count = 10;
-            float totalPrice = 20.95f;
-            char character = 'A';
-            string firstName = "Stevo";
-            bool isWorking = true;
-            Console.WriteLine(number);
-            Console.WriteLine(count);
-            Console.WriteLine(totalPrice);
-            Console.WriteLine(character);
-            Console.WriteLine(firstName);
-            Console.WriteLine(isWorking);
-
+            try
+            {
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Failed");
+            
+            }
         }
     }
 }
